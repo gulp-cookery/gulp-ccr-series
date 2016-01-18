15412b89bf49aef64f1d0676467ac5f3c6f25665
amobiz <amobiz.tw+github@gmail.com>
2016-01-18 22:15:05 +0800

    feature: add settle (--continue) support

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-/* eslint consistent-this: 0 */
 'use strict';
 
 /**
+ * Recipe:
+ * series
+ *
+ * Ingredients:
+ * async, asnyc-done
+ *
  * Note:
- *  Some kind of non-stream version of queue() stream recipe.
+ *  Some kind of non-stream version of queue() recipe.
  *
  * @param done
  */
@@ -11,14 +16,69 @@ function series(done) {
 	var async = require('async');
 	var asyncDone = require('async-done');
 
-	var context = this;
-	var tasks = context.tasks;
+	var gulp = this.gulp;
+	var config = this.config;
+	var helper = this.helper;
+	var tasks = this.tasks;
 
-	async.mapSeries(tasks, function (task, itemDone) {
-		asyncDone(function (taskDone) {
-			return task.run.call(context, taskDone);
-		}, itemDone);
-	}, done);
+	if (gulp._settle) {
+		async.mapSeries(tasks, runSettle, doneSettle);
+	} else {
+		async.mapSeries(tasks, runTask, done);
+	}
+
+	function runSettle(task, doneTask) {
+		return runTask(task, function (err, result) {
+			var state;
+
+			if (err) {
+				state = {
+					state: 'error',
+					value: err
+				};
+			} else {
+				state = {
+					state: 'success',
+					value: result
+				};
+			}
+			doneTask(null, state);
+		});
+	}
+
+	function doneSettle(err, result) {
+		var errors, results;
+
+		if (err) {
+			done(err, result);
+			return;
+		}
+
+		errors = null;
+		results = null;
+		if (result) {
+			errors = result.map(function (item) {
+				return item.state === 'error' ? item.value : null;
+			});
+			results = result.map(function (item) {
+				return item.state === 'success' ? item.value : null;
+			});
+		}
+		done(errors, results);
+	}
+
+	function runTask(task, doneTask) {
+		asyncDone(function (doneAsync) {
+			var context;
+
+			context = {
+				gulp: gulp,
+				helper: helper,
+				config: config
+			};
+			return task.call(context, doneAsync);
+		}, doneTask);
+	}
 }
 
 series.schema = {
